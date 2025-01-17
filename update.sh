c48df169e19e3deadc8889f3a678ff0e81182df9
SteavenGamerYT <omarhanykasban706@gmail.com>
2025-01-17 12:11:44 +0200

    Updated qrcodegen-cmake

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd x86_64
 rm -rf SteavenLinux.db.*
+rm -rf *-debug*
 repo-add --verify --sign SteavenLinux.db.tar.gz *.pkg.tar.zst
 git add .
 git commit -m "Updated SteavenLinux Repo Files"
